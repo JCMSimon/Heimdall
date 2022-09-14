dd8400ae5fe1145b60ffbdc0ddbfa188f5cbb738
Simon <simon1240.se@gmail.com>
2022-09-14 20:30:39 +0200

    Logs are now colored

--- lib/Logger.py
+++ lib/Logger.py
@@ -1,16 +1,32 @@
+from colorama import Fore,Style,init
+
 class Logger():
 	def __init__(self,prefix) -> None:
 		self.prefix = prefix
+		init()
 
 	def debug(self,text,debug = False):
 		if debug:
-			print(f"[{self.prefix} - DEBUG]")
+			print(f"{Fore.BLUE}[{self.prefix} - DEBUG] {text}{Style.RESET_ALL}")
 
 	def info(self,text):
-			print(f"[{self.prefix} - INFO]")
+			print(f"{Fore.CYAN}[{self.prefix} - INFO] {text}{Style.RESET_ALL}")
 
 	def warn(self,text):
-			print(f"[{self.prefix} - WARN]")
+			print(f"{Fore.YELLOW}[{self.prefix} - WARN] {text}{Style.RESET_ALL}")
 
 	def error(self,text):
-			print(f"[{self.prefix} - ERROR]")
+			print(f"{Fore.RED}[{self.prefix} - ERROR] {text}{Style.RESET_ALL}")
+
+if __name__ == "__main__":
+	class Example():
+		def __init__(self,debug) -> None:
+			logger = Logger("Test")
+			logger.debug("Testing Debug Message",debug)
+			logger.info("Testing Info Message")
+			logger.warn("Testing Warn Message")
+			logger.error("Testing Error Message")
+	print("Debug turned off:")
+	debugOff = Example(False)
+	print("Debug turned on:")
+	debugOn = Example(True)
