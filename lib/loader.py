a126765c41027c1ddca04f2d578407993bf62c13
JustCallMeSimon <no>
2022-10-16 00:28:19 +0200

    s

--- lib/loader.py
+++ lib/loader.py
@@ -1,11 +1,13 @@
 import dearpygui.dearpygui as dpg
 from screeninfo import get_monitors
+from lib.temp.windows import set_transparent_color
 # from lib.Logger import Logger
 
 class Loader:
 	def __init__(self) -> None:
 		# self.logger = Logger
 		self.initDPG()
+		self.initDPGThemes()
 		self.initMainWindow()
 		self.start()
 
@@ -13,7 +15,7 @@ class Loader:
 		dpg.create_context()
 		with dpg.texture_registry():
 			self.width, self.height, _, data = dpg.load_image("./assets/heimdall_logo.png")
-			self.Image = dpg.add_dynamic_texture(self.width, self.height, data,tag="test")
+			self.Image = dpg.add_dynamic_texture(self.width, self.height, data,tag="logo")
 		dpg.create_viewport(
 			title="Heimdall",              # Window Title (Also Application Title)
 			large_icon="assets/icon.ico",  # "favicon" for Application
@@ -27,6 +29,8 @@ class Loader:
 			)
 		dpg.setup_dearpygui()
 		dpg.show_viewport()
+		#Thanks to https://github.com/Atlamillias for this temporary workaround
+		set_transparent_color((0,0,0))
 		self.centerViewport()
 
 	def centerViewport(self):
@@ -34,18 +38,25 @@ class Loader:
 			if monitor.is_primary:
 				monitor_xd = monitor.width
 				monitor_yd = monitor.height
-		dpg.set_viewport_pos([
-			int(monitor_xd + self.width / 2),
-			int(monitor_yd + self.height / 2)
-			]
-		)
+		dpg.set_viewport_pos(pos=[
+			(monitor_xd - self.width) / 2,
+			(monitor_yd - self.height) / 2,
+		])
+
+	def initDPGThemes(self):
+		with dpg.theme() as mainWindowStyling:
+			with dpg.theme_component(dpg.mvAll):
+				dpg.add_theme_style(dpg.mvStyleVar_WindowBorderSize,0)
+				dpg.add_theme_style(dpg.mvStyleVar_WindowPadding,0,0)
+				dpg.add_theme_color(dpg.mvThemeCol_WindowBg,(0,0,0))
+				dpg.bind_theme(mainWindowStyling)
 
 	def initMainWindow(self):
 		# Main Window (Wrapper)
 		with dpg.window(
 			tag="mainWindow",
 			horizontal_scrollbar=False,
-			no_background=False,
+			no_background=True,
 			no_scrollbar=True,
 			no_title_bar=True,
 			no_collapse=True,
@@ -53,12 +64,10 @@ class Loader:
 			menubar=False,
 			no_close=True,
 			no_move=True,
-
 			):
-			dpg.add_image(texture_tag="test",width=100,height=100)
+			dpg.add_image(texture_tag="logo",width=self.width,height=self.height)
 		dpg.set_primary_window("mainWindow",True)
-		print(dpg.get_viewport_pos())
-
+		dpg.show_style_editor()
 
 	# Starts GUI
 	def start(self):
