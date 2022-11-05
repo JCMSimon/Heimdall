ceea90292f978f6dd8b8dc2e43a030635c2db455
Simon <simon1240.se@gmail.com>
2022-11-05 16:53:47 +0100

    struc

--- plugins/lib/_Node.py
+++ plugins/lib/_Node.py
@@ -1,22 +1,23 @@
 from src._Logger import Logger
 
 class Node():
-	def __init__(self,title,image=False) -> None:
+	def __init__(self,title) -> None:
 		"""
 		Node
 
 		Args:
 			title (_type_): Title of the Node
-			image (bool, string, optional): URL/Path to a image. Leave at False if no image
 		"""
 		self.logger = Logger("Node",debug=True)
 		self.data = {
 			"title":f"{title}",
 			"data":[],
+			"image":None,
 		}
-		if image != False:
-			self.data["image"] = image
 
 	def addDataField(datatype,data):
+		pass
+
+	def addImage(imagetype,imageUrl):
 		pass
 
