6ecf660c2d9ed43b154943e3bb87a815ea95dafd
Simon <simon1240.se@gmail.com>
2022-11-05 14:32:23 +0100

    f

--- plugins/lib/_Node.py
+++ plugins/lib/_Node.py
@@ -10,4 +10,13 @@ class Node():
 			image (bool, string, optional): URL/Path to a image. Leave at False if no image
 		"""
 		self.logger = Logger("Node",debug=True)
+		self.data = {
+			"title":f"{title}",
+			"data":[],
+		}
+		if image != False:
+			self.data["image"] = image
+
+	def addDataField(datatype,data):
+		pass
 
