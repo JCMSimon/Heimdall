5f088f03b026d4ace0b3c9326cd192323f8bacad
JustCallMeSimon <no>
2022-11-12 21:46:36 +0100

    a

--- Heimdall.py
+++ Heimdall.py
@@ -2,21 +2,21 @@ from multiprocessing import Process
 from sys import argv
 
 from src.Logger import Logger
-from src.Loader import Loader
+from src.loader import Loader
 from src.Core import Core
-from src.Gui import GUI
+from src.gui import GUI
 from plugins._PluginRegister import PluginRegister
 
 def defaultStart(debug):
 	logger = Logger("Start-up",debug=debug)
 	logger.debugMsg("Starting Loading Graphic")
-	loaderProcess = Process(target=startLoader,args=[debug]).start()
+	loaderProcess = Process(target=startLoader,args=[debug])
+	loaderProcess.start()
 	logger.debugMsg("Loading Plugins")
 	pluginRegister = PluginRegister(debug)
 	pluginNames = pluginRegister.getPluginNames()
 	logger.debugMsg("Closing Loading Graphic")
 	loaderProcess.terminate()
-	loaderProcess.close()
 	logger.debugMsg("Starting Main User Interface")
 	gui = GUI(pluginNames,debug=debug)
 	nodeEditor = gui.returnEditor()
