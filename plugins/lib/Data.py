f90c191f9b44edcbd534d86ed80301000abc5159
Simon <simon1240.se@gmail.com>
2022-11-14 18:49:36 +0100

    dot notation

--- plugins/lib/Data.py
+++ plugins/lib/Data.py
@@ -1,233 +1,330 @@
 class datapoints:
-	none = 0
-	name_title = 0
-	first_name = 0
-	middle_name = 0
-	last_name = 0
-	ethnicity = 0
-	nationality = 0
-	gender = 0
-	age = 0
-	age_day = 0
-	age_month = 0
-	age_year = 0
-	body_height = 0
-	body_weight = 0
-	eye_color = 0
-	date_of_birth = 0
-	date_of_birth_day = 0
-	date_of_birth_month = 0
-	date_of_birth_year = 0
-	education_level = 0
-	vehicle_license_plate = 0
-	vehicle_model_name = 0
-	family_mother_name_title = 0
-	family_mother_first_name = 0
-	family_mother_middle_name = 0
-	family_mother_last_name = 0
-	family_mother_ethnicity = 0
-	family_mother_nationality = 0
-	family_mother_gender = 0
-	family_mother_age = 0
-	family_mother_body_height = 0
-	family_mother_body_weight = 0
-	family_mother_eye_color = 0
-	family_mother_date_of_birth = 0
-	family_mother_date_of_birth_day = 0
-	family_mother_date_of_birth_month = 0
-	family_mother_date_of_birth_year = 0
-	family_mother_phone_number = 0
-	family_mother_education_level = 0
-	family_father_name_title = 0
-	family_father_first_name = 0
-	family_father_middle_name = 0
-	family_father_last_name = 0
-	family_father_ethnicity = 0
-	family_father_nationality = 0
-	family_father_gender = 0
-	family_father_age = 0
-	family_father_body_height = 0
-	family_father_body_weight = 0
-	family_father_eye_color = 0
-	family_father_date_of_birth = 0
-	family_father_date_of_birth_day = 0
-	family_father_date_of_birth_month = 0
-	family_father_date_of_birth_year = 0
-	family_father_phone_number = 0
-	family_father_education_level = 0
-	family_brother_name_title = 0
-	family_brother_first_name = 0
-	family_brother_middle_name = 0
-	family_brother_last_name = 0
-	family_brother_ethnicity = 0
-	family_brother_nationality = 0
-	family_brother_gender = 0
-	family_brother_age = 0
-	family_brother_body_height = 0
-	family_brother_body_weight = 0
-	family_brother_eye_color = 0
-	family_brother_date_of_birth = 0
-	family_brother_date_of_birth_day = 0
-	family_brother_date_of_birth_month = 0
-	family_brother_date_of_birth_year = 0
-	family_brother_phone_number = 0
-	family_brother_education_level = 0
-	family_sister_name_title = 0
-	family_sister_first_name = 0
-	family_sister_middle_name = 0
-	family_sister_last_name = 0
-	family_sister_ethnicity = 0
-	family_sister_nationality = 0
-	family_sister_gender = 0
-	family_sister_age = 0
-	family_sister_body_height = 0
-	family_sister_body_weight = 0
-	family_sister_eye_color = 0
-	family_sister_date_of_birth = 0
-	family_sister_date_of_birth_day = 0
-	family_sister_date_of_birth_month = 0
-	family_sister_date_of_birth_year = 0
-	family_sister_phone_number = 0
-	family_sister_education_level = 0
-	family_pet_type = 0
-	family_pet_name = 0
-	family_pet_gender = 0
-	family_pet_age = 0
-	family_pet_body_height = 0
-	family_pet_body_weight = 0
-	family_pet_date_of_birth = 0
-	family_pet_date_of_birth_day = 0
-	family_pet_date_of_birth_month = 0
-	family_pet_date_of_birth_year = 0
-	family_generic_name_title = 0
-	family_generic_first_name = 0
-	family_generic_middle_name = 0
-	family_generic_last_name = 0
-	family_generic_ethnicity = 0
-	family_generic_nationality = 0
-	family_generic_gender = 0
-	family_generic_age = 0
-	family_generic_age_day = 0
-	family_generic_age_month = 0
-	family_generic_age_year = 0
-	family_generic_body_height = 0
-	family_generic_body_weight = 0
-	family_generic_eye_color = 0
-	family_generic_date_of_birth = 0
-	family_generic_date_of_birth_day = 0
-	family_generic_date_of_birth_month = 0
-	family_generic_date_of_birth_year = 0
-	family_generic_phone_number = 0
-	personal_website_domain = 0
-	email_hash = 0
-	generic_email = 0
-	gmail_email = 0
-	yahoo_email = 0
-	hotmail_email = 0
-	outlook_email = 0
-	web_de_email = 0
-	proton_email = 0
-	apple_email = 0
-	apple_email = 0
-	phone_number = 0
-	fax_number = 0
-	password = 0
-	password_hash = 0
-	numerical_pin_number = 0
+	undefined = 0
 	ip_address = 0
-	home_property_country = 0
-	home_property_address = 0
-	home_property_state = 0
-	home_property_region = 0
-	home_property_county = 0
-	home_property_city = 0
-	home_property_zip_code = 0
-	home_property_street_name = 0
-	home_property_house_number = 0
-	home_property_apartment_number = 0
-	home_property_apartment_longitude = 0
-	home_property_apartment_latitude = 0
-	generic_property_country = 0
-	generic_property_address = 0
-	generic_property_state = 0
-	generic_property_region = 0
-	generic_property_county = 0
-	generic_property_city = 0
-	generic_property_zip_code = 0
-	generic_property_street_name = 0
-	generic_property_house_number = 0
-	generic_property_apartment_number = 0
-	generic_property_apartment_longitude = 0
-	generic_property_apartment_latitude = 0
-	birth_place_name = 0
-	birth_place_address = 0
-	birth_place_address_state = 0
-	birth_place_address_region = 0
-	birth_place_address_county = 0
-	birth_place_address_city = 0
-	birth_place_address_zip_code = 0
-	birth_place_address_street_name = 0
-	birth_place_address_longitude = 0
-	birth_place_address_latitude = 0
-	school_name = 0
-	school_address = 0
-	school_address_state = 0
-	school_address_region = 0
-	school_address_county = 0
-	school_address_city = 0
-	school_address_zip_code = 0
-	school_address_street_name = 0
-	school_address_longitude = 0
-	school_address_latitude = 0
-	workplace_name = 0
-	workplace_address = 0
-	workplace_address_state = 0
-	workplace_address_region = 0
-	workplace_address_county = 0
-	workplace_address_city = 0
-	workplace_address_zip_code = 0
-	workplace_address_street_name = 0
-	workplace_address_longitude = 0
-	workplace_address_latitude = 0
-	credit_card_number = 0
-	credit_card_month = 0
-	credit_card_year = 0
-	credit_card_cvc = 0
-	credit_card_name = 0
-	social_security_number = 0
-	generic_username = 0
-	facebook_username = 0
-	twitter_username = 0
-	youtube_username = 0
-	instagram_username = 0
-	pinterest_username = 0
-	tiktok_username = 0
-	reddit_username = 0
-	snapchat_username = 0
-	discord_username = 0
-	steam_username = 0
-	twitch_username = 0
-	telegram_username = 0
-	google_username = 0
-	google_plus_username = 0
-	vk_username = 0
-	generic_socialmedia_account = 0
-	facebook_account = 0
-	twitter_account = 0
-	youtube_account = 0
-	instagram_account = 0
-	pinterest_account = 0
-	tiktok_account = 0
-	reddit_account = 0
-	snapchat_account = 0
-	discord_account = 0
-	steam_account = 0
-	twitch_account = 0
-	telegram_account = 0
-	google_account = 0
-	google_plus_account = 0
-	vk_account = 0
-	# Images
-	image_generic = 0
-	image_face = 0
-	image_id = 0
+	class name:
+		title = 0
+		first_name = 0
+		middle_name = 0
+		last_name = 0
+	class age:
+		age_in_years = 0
+		class date_of_birth:
+			date_of_birth = 0
+			day = 0
+			month = 0
+			year = 0
+	class person:
+		ethnicity = 0
+		nationality = 0
+	class body:
+		height = 0
+		weight = 0
+		eye_color = 0
+		skin_color = 0
+		hair_color = 0
+		gender = 0
+	class education:
+		level = 0
+		class school:
+			school_name = 0
+			class address:
+				address = 0
+				city = 0
+				county = 0
+				latitude = 0
+				longitude = 0
+				region = 0
+				state = 0
+				street_name = 0
+				zip_code = 0
+	class vehicle:
+		type = 0
+		license_plate = 0
+		tail_number = 0
+		model = 0
+		build_year = 0
+	class family:
+		class father:
+			class name:
+				title = 0
+				first_name = 0
+				middle_name = 0
+				last_name = 0
+			class age:
+				age_in_years = 0
+				date_of_birth = 0
+				date_of_birth_day = 0
+				date_of_birth_month = 0
+				date_of_birth_year = 0
+			class body:
+				height = 0
+				weight = 0
+				eye_color = 0
+				skin_color = 0
+				hair_color = 0
+				gender = 0
+			class education:
+				education_level = 0
+			class person:
+				ethnicity = 0
+				nationality = 0
+			class contact:
+				phone_number = 0
+				email = 0
+				fax_number = 0
+		class mother:
+			class name:
+				title = 0
+				first_name = 0
+				middle_name = 0
+				last_name = 0
+			class age:
+				age_in_years = 0
+				date_of_birth = 0
+				date_of_birth_day = 0
+				date_of_birth_month = 0
+				date_of_birth_year = 0
+			class body:
+				height = 0
+				weight = 0
+				eye_color = 0
+				skin_color = 0
+				hair_color = 0
+				gender = 0
+			class education:
+				education_level = 0
+			class person:
+				ethnicity = 0
+				nationality = 0
+			class contact:
+				phone_number = 0
+				email = 0
+				fax_number = 0
+		class brother:
+			class name:
+				title = 0
+				first_name = 0
+				middle_name = 0
+				last_name = 0
+			class age:
+				age_in_years = 0
+				date_of_birth = 0
+				date_of_birth_day = 0
+				date_of_birth_month = 0
+				date_of_birth_year = 0
+			class body:
+				height = 0
+				weight = 0
+				eye_color = 0
+				skin_color = 0
+				hair_color = 0
+				gender = 0
+			class education:
+				education_level = 0
+			class person:
+				ethnicity = 0
+				nationality = 0
+			class contact:
+				phone_number = 0
+				email = 0
+				fax_number = 0
+		class sister:
+			class name:
+				title = 0
+				first_name = 0
+				middle_name = 0
+				last_name = 0
+			class age:
+				age_in_years = 0
+				date_of_birth = 0
+				date_of_birth_day = 0
+				date_of_birth_month = 0
+				date_of_birth_year = 0
+			class body:
+				height = 0
+				weight = 0
+				eye_color = 0
+				skin_color = 0
+				hair_color = 0
+				gender = 0
+			class education:
+				education_level = 0
+			class person:
+				ethnicity = 0
+				nationality = 0
+			class contact:
+				phone_number = 0
+				email = 0
+				fax_number = 0
+		class pet:
+			type = 0
+			color = 0
+			class name:
+				title = 0
+				first_name = 0
+				middle_name = 0
+				last_name = 0
+			class age:
+				age_in_years = 0
+				date_of_birth = 0
+				date_of_birth_day = 0
+				date_of_birth_month = 0
+				date_of_birth_year = 0
+			class body:
+				height = 0
+				length = 0
+				weight = 0
+				gender = 0
+		class generic:
+			class name:
+				title = 0
+				first_name = 0
+				middle_name = 0
+				last_name = 0
+			class age:
+				age_in_years = 0
+				date_of_birth = 0
+				date_of_birth_day = 0
+				date_of_birth_month = 0
+				date_of_birth_year = 0
+			class body:
+				height = 0
+				weight = 0
+				eye_color = 0
+				skin_color = 0
+				hair_color = 0
+				gender = 0
+			class education:
+				education_level = 0
+			class person:
+				ethnicity = 0
+				nationality = 0
+			class contact:
+				phone_number = 0
+				email = 0
+				fax_number = 0
+	class personal_website:
+		name = 0
+		domain = 0
+		ip = 0
+		subdomain = 0
+		dns_server = 0
+	class email:
+		apple = 0
+		generic = 0
+		gmail = 0
+		hotmail = 0
+		outlook = 0
+		proton = 0
+		web_de = 0
+		yahoo = 0
+	class hashes:
+		email = 0
+		password = 0
+		generic = 0
+	class credentials:
+		numerical_pin_number = 0
+		password = 0
+	class contact:
+		fax_number = 0
+		phone_number = 0
+	class property:
+		class home:
+			address = 0
+			apartment_latitude = 0
+			apartment_longitude = 0
+			apartment_number = 0
+			city = 0
+			country = 0
+			county = 0
+			house_number = 0
+			region = 0
+			state = 0
+			street_name = 0
+			zip_code = 0
+		class generic:
+			address = 0
+			apartment_latitude = 0
+			apartment_longitude = 0
+			apartment_number = 0
+			city = 0
+			country = 0
+			county = 0
+			house_number = 0
+			region = 0
+			state = 0
+			street_name = 0
+			zip_code = 0
+	class work:
+		title = 0
+		class workplace:
+			company_name = 0
+			class address:
+				address = 0
+				city = 0
+				county = 0
+				latitude = 0
+				longitude = 0
+				region = 0
+				state = 0
+				street_name = 0
+				zip_code = 0
+	class birth_place:
+		name = 0
+		class address:
+			address = 0
+			address_city = 0
+			address_county = 0
+			address_latitude = 0
+			address_longitude = 0
+			address_region = 0
+			address_state = 0
+			address_street_name = 0
+			address_zip_code = 0
+	class sensetive_data:
+		social_security_number = 0
+		class credit_card:
+			name = 0
+			expiry_date = 0
+			expiry_month = 0
+			expiry_number = 0
+			expiry_year = 0
+			cvc = 0
+	class username:
+		discord = 0
+		facebook = 0
+		generic = 0
+		google_plus = 0
+		google = 0
+		instagram = 0
+		pinterest = 0
+		reddit = 0
+		snapchat = 0
+		steam = 0
+		telegram = 0
+		tiktok = 0
+		twitch = 0
+		twitter = 0
+		vk = 0
+		youtube = 0
+	class account:
+		discord = 0
+		facebook = 0
+		generic = 0
+		google = 0
+		google_plus = 0
+		instagram = 0
+		pinterest = 0
+		reddit = 0
+		snapchat = 0
+		steam = 0
+		telegram = 0
+		tiktok = 0
+		twitch = 0
+		twitter = 0
+		vk = 0
+		youtube = 0
+	class image:
+		generic = 0
+		face = 0
+		id = 0
