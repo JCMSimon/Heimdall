9059bb9d99e890e31b4e9695079bde6dc319e25f
Simon <simon1240.se@gmail.com>
2022-11-18 18:57:15 +0100

    Search Loop v1

--- src/Core.py
+++ src/Core.py
@@ -1,4 +1,5 @@
 from src.NodeInterface import NodeInterface
+from plugins.lib.Node import Node
 
 class Core():
 	def __init__(self,pluginRegister,nodeEditor,debug=False) -> None:
@@ -6,7 +7,35 @@ class Core():
 		self.nodeInterFace = NodeInterface(nodeEditor,debug=debug)
 
 	def search(self,datatype,keyword):
-		# pluginsToRun = self.pluginRegister.getPluginsForType(datatype)
-		data = self.pluginRegister.runPlugin(datatype,keyword)
-		for node in data:
-			print(node.data)
+		# create parent node with search as text
+		parent = Node("Search here")
+		results = self.pluginRegister.runPlugin(datatype,keyword)
+		#TODO later on it should already ask for all plugins to be ran that accept the type that is input
+		#TODO have plugins define a default input type
+		# add all plugin results as children to parent node
+		parent._children.extend(results)
+		# create todo list
+		todo = []
+		# for child in children:
+		for child in parent._children:
+		#	for data element in child
+			for pair in child.data["data"]:
+				for datatype,data in pair:
+		#		run plugin and add result to result list
+					children = self.pluginRegister.runPlugin("doesent","matter")
+		#	add results as children to ""child""
+			child._children.extend(children)
+		#	save children to TODO list
+			todo.extend(children)
+		# for child in TODO list:
+		for node in todo:
+		#	for data element in child
+			for pair in child.data["data"]:
+				for datatype,data in pair:
+		#		run plugin and add result to result list
+					children = self.pluginRegister.runPlugin("doesent","matter")
+		#	add results as children
+			node._children.extend(children)
+		#	save children to TODO list at the back
+			todo.extend(children)
+		pass
