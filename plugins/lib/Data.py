fdeebf6ce8d03f026261dc18a688eb355a50176c
Simon <simon1240.se@gmail.com>
2022-11-19 15:36:13 +0100

    i hate my life

--- plugins/lib/Data.py
+++ plugins/lib/Data.py
@@ -1,332 +1,334 @@
 class datapoints:
-	undefined = 0
-	ip_address = 0
+	undefined = "dp.undefined"
+	ip_address = "dp.ip_address"
 	class _internal:
-		is_root_node = 0
+		is_root_node = "dp._internal.is_root_node"
 	class name:
-		title = 0
-		first_name = 0
-		middle_name = 0
-		last_name = 0
+		title = "dp.name.title"
+		first_name = "dp.name.first_name"
+		middle_name = "dp.name.middle_name"
+		last_name = "dp.name.last_name"
 	class age:
-		age_in_years = 0
+		age_in_years = "dp.age.age_in_years"
 		class date_of_birth:
-			date_of_birth = 0
-			day = 0
-			month = 0
-			year = 0
+			date_of_birth = "dp.age.date_of_birth.date_of_birth"
+			day = "dp.age.date_of_birth.day"
+			month = "dp.age.date_of_birth.month"
+			year = "dp.age.date_of_birth.year"
 	class person:
-		ethnicity = 0
-		nationality = 0
+		ethnicity = "dp.person.ethnicity"
+		nationality = "dp.person.nationality"
 	class body:
-		height = 0
-		weight = 0
-		eye_color = 0
-		skin_color = 0
-		hair_color = 0
-		gender = 0
+		height = "dp.body.height"
+		weight = "dp.body.weight"
+		eye_color = "dp.body.eye_color"
+		skin_color = "dp.body.skin_color"
+		hair_color = "dp.body.hair_color"
+		gender = "dp.body.gender"
 	class education:
-		level = 0
+		level = "dp.education.level"
 		class school:
-			school_name = 0
+			school_name = "dp.education.school.school_name"
 			class address:
-				address = 0
-				city = 0
-				county = 0
-				latitude = 0
-				longitude = 0
-				region = 0
-				state = 0
-				street_name = 0
-				zip_code = 0
+				address = "dp.education.school.adress.address"
+				city = "dp.education.school.adress.city"
+				county = "dp.education.school.adress.county"
+				latitude = "dp.education.school.adress.latitude"
+				longitude = "dp.education.school.adress.longitude"
+				region = "dp.education.school.adress.region"
+				state = "dp.education.school.adress.state"
+				street_name = "dp.education.school.adress.street_name"
+				zip_code = "dp.education.school.adress.zip_code"
 	class vehicle:
-		type = 0
-		license_plate = 0
-		tail_number = 0
-		model = 0
-		build_year = 0
+		type = "dp.vehicle.type"
+		license_plate = "dp.vehicle.license_plate"
+		tail_number = "dp.vehicle.tail_number"
+		model = "dp.vehicle.model"
+		build_year = "dp.vehicle.build_year"
 	class family:
 		class father:
 			class name:
-				title = 0
-				first_name = 0
-				middle_name = 0
-				last_name = 0
+				title = "dp.family.father.name.title"
+				first_name = "dp.family.father.name.first_name"
+				middle_name = "dp.family.father.name.middle_name"
+				last_name = "dp.family.father.name.last_name"
 			class age:
-				age_in_years = 0
-				date_of_birth = 0
-				date_of_birth_day = 0
-				date_of_birth_month = 0
-				date_of_birth_year = 0
+				age_in_years = "dp.family.father.age.age_in_years"
+				date_of_birth = "dp.family.father.age.date_of_birth"
+				date_of_birth_day = "dp.family.father.age.date_of_birth_day"
+				date_of_birth_month = "dp.family.father.age.date_of_birth_month"
+				date_of_birth_year = "dp.family.father.age.date_of_birth_year"
 			class body:
-				height = 0
-				weight = 0
-				eye_color = 0
-				skin_color = 0
-				hair_color = 0
-				gender = 0
+				height = "dp.family.father.body.height"
+				weight = "dp.family.father.body.weight"
+				eye_color = "dp.family.father.body.eye_color"
+				skin_color = "dp.family.father.body.skin_color"
+				hair_color = "dp.family.father.body.hair_color"
+				gender = "dp.family.father.body.gender"
 			class education:
-				education_level = 0
+				level = "dp.family.father.education.level"
 			class person:
-				ethnicity = 0
-				nationality = 0
+				ethnicity = "dp.family.father.person.ethnicity"
+				nationality = "dp.family.father.person.nationality"
 			class contact:
-				phone_number = 0
-				email = 0
-				fax_number = 0
+				phone_number = "dp.family.father.contact.phone_number"
+				email = "dp.family.father.contact.email"
+				fax_number = "dp.family.father.contact.fax_number"
 		class mother:
 			class name:
-				title = 0
-				first_name = 0
-				middle_name = 0
-				last_name = 0
+				title = "dp.family.mother.name.title"
+				first_name = "dp.family.mother.name.first_name"
+				middle_name = "dp.family.mother.name.middle_name"
+				last_name = "dp.family.mother.name.last_name"
 			class age:
-				age_in_years = 0
-				date_of_birth = 0
-				date_of_birth_day = 0
-				date_of_birth_month = 0
-				date_of_birth_year = 0
+				age_in_years = "dp.family.mother.age.age_in_years"
+				date_of_birth = "dp.family.mother.age.date_of_birth"
+				date_of_birth_day = "dp.family.mother.age.date_of_birth_day"
+				date_of_birth_month = "dp.family.mother.age.date_of_birth_month"
+				date_of_birth_year = "dp.family.mother.age.date_of_birth_year"
 			class body:
-				height = 0
-				weight = 0
-				eye_color = 0
-				skin_color = 0
-				hair_color = 0
-				gender = 0
+				height = "dp.family.mother.body.height"
+				weight = "dp.family.mother.body.weight"
+				eye_color = "dp.family.mother.body.eye_color"
+				skin_color = "dp.family.mother.body.skin_color"
+				hair_color = "dp.family.mother.body.hair_color"
+				gender = "dp.family.mother.body.gender"
 			class education:
-				education_level = 0
+				level = "dp.family.mother.education.level"
 			class person:
-				ethnicity = 0
-				nationality = 0
+				ethnicity = "dp.family.mother.person.ethnicity"
+				nationality = "dp.family.mother.person.nationality"
 			class contact:
-				phone_number = 0
-				email = 0
-				fax_number = 0
+				phone_number = "dp.family.mother.contact.phone_number"
+				email = "dp.family.mother.contact.email"
+				fax_number = "dp.family.mother.contact.fax_number"
 		class brother:
 			class name:
-				title = 0
-				first_name = 0
-				middle_name = 0
-				last_name = 0
+				title = "dp.family.brother.name.title"
+				first_name = "dp.family.brother.name.first_name"
+				middle_name = "dp.family.brother.name.middle_name"
+				last_name = "dp.family.brother.name.last_name"
 			class age:
-				age_in_years = 0
-				date_of_birth = 0
-				date_of_birth_day = 0
-				date_of_birth_month = 0
-				date_of_birth_year = 0
+				age_in_years = "dp.family.brother.age.age_in_years"
+				date_of_birth = "dp.family.brother.age.date_of_birth"
+				date_of_birth_day = "dp.family.brother.age.date_of_birth_day"
+				date_of_birth_month = "dp.family.brother.age.date_of_birth_month"
+				date_of_birth_year = "dp.family.brother.age.date_of_birth_year"
 			class body:
-				height = 0
-				weight = 0
-				eye_color = 0
-				skin_color = 0
-				hair_color = 0
-				gender = 0
+				height = "dp.family.brother.body.height"
+				weight = "dp.family.brother.body.weight"
+				eye_color = "dp.family.brother.body.eye_color"
+				skin_color = "dp.family.brother.body.skin_color"
+				hair_color = "dp.family.brother.body.hair_color"
+				gender = "dp.family.brother.body.gender"
 			class education:
-				education_level = 0
+				level = "dp.family.brother.education.level"
 			class person:
-				ethnicity = 0
-				nationality = 0
+				ethnicity = "dp.family.brother.person.ethnicity"
+				nationality = "dp.family.brother.person.nationality"
 			class contact:
-				phone_number = 0
-				email = 0
-				fax_number = 0
+				phone_number = "dp.family.brother.contact.phone_number"
+				email = "dp.family.brother.contact.email"
+				fax_number = "dp.family.brother.contact.fax_number"
 		class sister:
 			class name:
-				title = 0
-				first_name = 0
-				middle_name = 0
-				last_name = 0
+				title = "dp.family.sister.name.title"
+				first_name = "dp.family.sister.name.first_name"
+				middle_name = "dp.family.sister.name.middle_name"
+				last_name = "dp.family.sister.name.last_name"
 			class age:
-				age_in_years = 0
-				date_of_birth = 0
-				date_of_birth_day = 0
-				date_of_birth_month = 0
-				date_of_birth_year = 0
+				age_in_years = "dp.family.sister.age.age_in_years"
+				date_of_birth = "dp.family.sister.age.date_of_birth"
+				date_of_birth_day = "dp.family.sister.age.date_of_birth_day"
+				date_of_birth_month = "dp.family.sister.age.date_of_birth_month"
+				date_of_birth_year = "dp.family.sister.age.date_of_birth_year"
 			class body:
-				height = 0
-				weight = 0
-				eye_color = 0
-				skin_color = 0
-				hair_color = 0
-				gender = 0
+				height = "dp.family.sister.body.height"
+				weight = "dp.family.sister.body.weight"
+				eye_color = "dp.family.sister.body.eye_color"
+				skin_color = "dp.family.sister.body.skin_color"
+				hair_color = "dp.family.sister.body.hair_color"
+				gender = "dp.family.sister.body.gender"
 			class education:
-				education_level = 0
+				level = "dp.family.sister.education.level"
 			class person:
-				ethnicity = 0
-				nationality = 0
+				ethnicity = "dp.family.sister.person.ethnicity"
+				nationality = "dp.family.sister.person.nationality"
 			class contact:
-				phone_number = 0
-				email = 0
-				fax_number = 0
+				phone_number = "dp.family.sister.contact.phone_number"
+				email = "dp.family.sister.contact.email"
+				fax_number = "dp.family.sister.contact.fax_number"
 		class pet:
-			type = 0
-			color = 0
+			type = "dp.family.pet.type"
+			color = "dp.family.pet.color"
 			class name:
-				title = 0
-				first_name = 0
-				middle_name = 0
-				last_name = 0
+				title = "dp.family.pet.name.title"
+				first_name = "dp.family.pet.name.first_name"
+				middle_name = "dp.family.pet.name.middle_name"
+				last_name = "dp.family.pet.name.last_name"
 			class age:
-				age_in_years = 0
-				date_of_birth = 0
-				date_of_birth_day = 0
-				date_of_birth_month = 0
-				date_of_birth_year = 0
+				age_in_years = "dp.family.pet.age.age_in_years"
+				date_of_birth = "dp.family.pet.age.date_of_birth"
+				date_of_birth_day = "dp.family.pet.age.date_of_birth_day"
+				date_of_birth_month = "dp.family.pet.age.date_of_birth_month"
+				date_of_birth_year = "dp.family.pet.age.date_of_birth_year"
 			class body:
-				height = 0
-				length = 0
-				weight = 0
-				gender = 0
+				height = "dp.family.pet.body.height"
+				length = "dp.family.pet.body.length"
+				weight = "dp.family.pet.body.weight"
+				gender = "dp.family.pet.body.gender"
 		class generic:
+			type = "dp.family.generic.type"
 			class name:
-				title = 0
-				first_name = 0
-				middle_name = 0
-				last_name = 0
+				title = "dp.family.generic.name.title"
+				first_name = "dp.family.generic.name.first_name"
+				middle_name = "dp.family.generic.name.middle_name"
+				last_name = "dp.family.generic.name.last_name"
 			class age:
-				age_in_years = 0
-				date_of_birth = 0
-				date_of_birth_day = 0
-				date_of_birth_month = 0
-				date_of_birth_year = 0
+				age_in_years = "dp.family.generic.age.age_in_years"
+				date_of_birth = "dp.family.generic.age.date_of_birth"
+				date_of_birth_day = "dp.family.generic.age.date_of_birth_day"
+				date_of_birth_month = "dp.family.generic.age.date_of_birth_month"
+				date_of_birth_year = "dp.family.generic.age.date_of_birth_year"
 			class body:
-				height = 0
-				weight = 0
-				eye_color = 0
-				skin_color = 0
-				hair_color = 0
-				gender = 0
+				height = "dp.family.generic.body.height"
+				weight = "dp.family.generic.body.weight"
+				eye_color = "dp.family.generic.body.eye_color"
+				skin_color = "dp.family.generic.body.skin_color"
+				hair_color = "dp.family.generic.body.hair_color"
+				gender = "dp.family.generic.body.gender"
 			class education:
-				education_level = 0
+				level = "dp.family.generic.education.level"
 			class person:
-				ethnicity = 0
-				nationality = 0
+				ethnicity = "dp.family.generic.person.ethnicity"
+				nationality = "dp.family.generic.person.nationality"
 			class contact:
-				phone_number = 0
-				email = 0
-				fax_number = 0
+				phone_number = "dp.family.generic.contact.phone_number"
+				email = "dp.family.generic.contact.email"
+				fax_number = "dp.family.generic.contact.fax_number"
 	class personal_website:
-		name = 0
-		domain = 0
-		ip = 0
-		subdomain = 0
-		dns_server = 0
+		name = "dp.personal_website.name"
+		domain = "dp.personal_website.domain"
+		ip = "dp.personal_website.ip"
+		subdomain = "dp.personal_website.subdomain"
+		dns_server = "dp.personal_website.dns_server"
 	class email:
-		apple = 0
-		generic = 0
-		gmail = 0
-		hotmail = 0
-		outlook = 0
-		proton = 0
-		web_de = 0
-		yahoo = 0
+		apple = "dp.email.apple"
+		generic = "dp.email.generic"
+		gmail = "dp.email.gmail"
+		hotmail = "dp.email.hotmail"
+		outlook = "dp.email.outlook"
+		proton = "dp.email.proton"
+		web_de = "dp.email.web_de"
+		yahoo = "dp.email.yahoo"
 	class hashes:
-		email = 0
-		password = 0
-		generic = 0
+		email = "dp.hashed.email"
+		password = "dp.hashed.password"
+		generic = "dp.hashed.generic"
 	class credentials:
-		numerical_pin_number = 0
-		password = 0
+		numerical_pin_number = "dp.credentials.numerical_pin_number"
+		password = "dp.credentials.password"
 	class contact:
-		fax_number = 0
-		phone_number = 0
+		fax_number = "dp.contact.fax_number"
+		phone_number = "dp.contact.phone_number"
 	class property:
 		class home:
-			address = 0
-			apartment_latitude = 0
-			apartment_longitude = 0
-			apartment_number = 0
-			city = 0
-			country = 0
-			county = 0
-			house_number = 0
-			region = 0
-			state = 0
-			street_name = 0
-			zip_code = 0
+			address = "dp.property.home.address"
+			latitude = "dp.property.home.latitude"
+			longitude = "dp.property.home.longitude"
+			apartment_number = "dp.property.home.apartment_number"
+			city = "dp.property.home.city"
+			country = "dp.property.home.country"
+			county = "dp.property.home.county"
+			house_number = "dp.property.home.house_number"
+			region = "dp.property.home.region"
+			state = "dp.property.home.state"
+			street_name = "dp.property.home.street_name"
+			zip_code = "dp.property.home.zip_code"
 		class generic:
-			address = 0
-			apartment_latitude = 0
-			apartment_longitude = 0
-			apartment_number = 0
-			city = 0
-			country = 0
-			county = 0
-			house_number = 0
-			region = 0
-			state = 0
-			street_name = 0
-			zip_code = 0
+			type = "dp.property.generic.type"
+			address = "dp.property.generic.address"
+			latitude = "dp.property.generic.apartment_latitude"
+			longitude = "dp.property.generic.apartment_longitude"
+			apartment_number = "dp.property.generic.apartment_number"
+			city = "dp.property.generic.city"
+			country = "dp.property.generic.country"
+			county = "dp.property.generic.county"
+			house_number = "dp.property.generic.house_number"
+			region = "dp.property.generic.region"
+			state = "dp.property.generic.state"
+			street_name = "dp.property.generic.street_name"
+			zip_code = "dp.property.generic.zip_code"
 	class work:
-		title = 0
+		title = "dp.work.title"
 		class workplace:
-			company_name = 0
+			company_name = "dp.work.workplace.company_name"
 			class address:
-				address = 0
-				city = 0
-				county = 0
-				latitude = 0
-				longitude = 0
-				region = 0
-				state = 0
-				street_name = 0
-				zip_code = 0
+				address = "dp.work.workplace.address.address"
+				city = "dp.work.workplace.address.city"
+				county = "dp.work.workplace.address.county"
+				latitude = "dp.work.workplace.address.latitude"
+				longitude = "dp.work.workplace.address.longitude"
+				region = "dp.work.workplace.address.region"
+				state = "dp.work.workplace.address.state"
+				street_name = "dp.work.workplace.address.street_name"
+				zip_code = "dp.work.workplace.address.zip_code"
 	class birth_place:
 		name = 0
 		class address:
-			address = 0
-			address_city = 0
-			address_county = 0
-			address_latitude = 0
-			address_longitude = 0
-			address_region = 0
-			address_state = 0
-			address_street_name = 0
-			address_zip_code = 0
+			address = "dp.birth_place.address.address"
+			city = "dp.birth_place.address.city"
+			county = "dp.birth_place.address.county"
+			latitude = "dp.birth_place.address.latitude"
+			longitude = "dp.birth_place.address.longitude"
+			region = "dp.birth_place.address.region"
+			state = "dp.birth_place.address.state"
+			street_name = "dp.birth_place.address.street_name"
+			zip_code = "dp.birth_place.address.zip_code"
 	class sensetive_data:
-		social_security_number = 0
+		social_security_number = "dp.sensetive_data.social_security_number"
 		class credit_card:
-			name = 0
-			expiry_date = 0
-			expiry_month = 0
-			expiry_number = 0
-			expiry_year = 0
-			cvc = 0
+			name = "dp.sensetive_data.credit_card.name"
+			expiry_date = "dp.sensetive_data.credit_card.expiry_date"
+			expiry_month = "dp.sensetive_data.credit_card.expiry_month"
+			expiry_number = "dp.sensetive_data.credit_card.expiry_number"
+			expiry_year = "dp.sensetive_data.credit_card.expiry_year"
+			cvc = "dp.sensetive_data.credit_card.cvc"
 	class username:
-		discord = 0
-		facebook = 0
-		generic = 0
-		google_plus = 0
-		google = 0
-		instagram = 0
-		pinterest = 0
-		reddit = 0
-		snapchat = 0
-		steam = 0
-		telegram = 0
-		tiktok = 0
-		twitch = 0
-		twitter = 0
-		vk = 0
-		youtube = 0
+		discord = "dp.username.discord"
+		facebook = "dp.username.facebook"
+		generic = "dp.username.generic"
+		google_plus = "dp.username.google_plus"
+		google = "dp.username.google"
+		instagram = "dp.username.instagram"
+		pinterest = "dp.username.pinterest"
+		reddit = "dp.username.reddit"
+		snapchat = "dp.username.snapchat"
+		steam = "dp.username.steam"
+		telegram = "dp.username.telegram"
+		tiktok = "dp.username.tiktok"
+		twitch = "dp.username.twitch"
+		twitter = "dp.username.twitter"
+		vk = "dp.username.vk"
+		youtube = "dp.username.youtube"
 	class account:
-		discord = 0
-		facebook = 0
-		generic = 0
-		google = 0
-		google_plus = 0
-		instagram = 0
-		pinterest = 0
-		reddit = 0
-		snapchat = 0
-		steam = 0
-		telegram = 0
-		tiktok = 0
-		twitch = 0
-		twitter = 0
-		vk = 0
-		youtube = 0
+		discord = "dp.account.discord"
+		facebook = "dp.account.facebook"
+		generic = "dp.account.generic"
+		google = "dp.account.google"
+		google_plus = "dp.account.google_plus"
+		instagram = "dp.account.instagram"
+		pinterest = "dp.account.pinterest"
+		reddit = "dp.account.reddit"
+		snapchat = "dp.account.snapchat"
+		steam = "dp.account.steam"
+		telegram = "dp.account.telegram"
+		tiktok = "dp.account.tiktok"
+		twitch = "dp.account.twitch"
+		twitter = "dp.account.twitter"
+		vk = "dp.account.vk"
+		youtube = "dp.account.youtube"
 	class image:
-		generic = 0
-		face = 0
-		id = 0
+		generic = "dp.image.generic"
+		face = "dp.image.face"
+		id = "dp.image.id"
