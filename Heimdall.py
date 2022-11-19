b1dfad14aef349381b7524f830feeb86c7de1d12
AlmosYT <almosytpixelplace@gmail.com>
2022-11-19 13:15:51 +0100

    Add code to create updateconfig.json if it doesn't exist

--- Heimdall.py
+++ Heimdall.py
@@ -1,8 +1,9 @@
 from multiprocessing import Process
 from sys import argv
+import os
 
 from src.Logger import Logger
-from src.Loader import Loader
+#from src.Loader import Loader
 from src.Core import Core
 from src.gui import GUI
 from plugins._PluginRegister import PluginRegister
@@ -12,6 +13,12 @@ def defaultStart(debug):
 	logger.debugMsg("Starting Loading Graphic")
 	loaderProcess = Process(target=startLoader,args=[debug])
 	loaderProcess.start()
+	logger.debugMsg("Checking for update config")
+	result = CheckUpdateConfig()
+	if result:
+		logger.debugMsg("Update config found")
+	else:
+		logger.debugMsg("Update config not found, created one")
 	logger.debugMsg("Loading API Keys")
 	logger.debugMsg("Loading Plugins")
 	pluginRegister = PluginRegister(debug)
@@ -26,6 +33,13 @@ def defaultStart(debug):
 
 def startLoader(debug):
 	_ = Loader(debug=debug)
+
+def CheckUpdateConfig():
+	if not os.path.isfile("updateconfig.json"):
+		with open("updateconfig.json","w") as f:
+			f.write("{\n\t\"lastUpdated\": \"2020-08-01T00:00:00\",\n\t\"ignoreMinorUpdates\": false,\n\t\"ignoreMajorUpdates\": false\n}")
+			return False
+	return True
 
 if __name__ == "__main__":
 	debug = bool("--debug" in argv[1:])
