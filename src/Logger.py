eca5ca6a0eadbc41ba69ae28754062328fe474ac
JustCallMeSimon <no>
2022-11-26 23:25:33 +0100

    Final till 1.0

--- src/Logger.py
+++ src/Logger.py
@@ -1,26 +1,67 @@
-from colorama import Fore,Style,init
+from colorama import Fore,Style
+from colorama import init as initColors
 from time import localtime
 
 class Logger():
+	"""
+	Logging Utility for Plugins
+	"""
 	def __init__(self,prefix,debug=False) -> None:
+		"""
+		This function initializes the class with the prefix and debug variables
+
+		Args:
+		  prefix: The prefix of the logger.
+		  debug: If set to True, it will print out the debug messages. Defaults to False
+		"""
 		self.debug = debug
 		self.prefix = prefix
-		init()
+		initColors()
 
 	def debugMsg(self,text):
+		"""
+		It prints a message to the console if the debug flag is set to true
+
+		Args:
+		  text: The text to be printed
+		"""
 		if self.debug:
 			print(f"{Fore.BLUE}[{self.getTimeStamp()} - {self.prefix} - DEBUG] {text}{Style.RESET_ALL}")
 
 	def infoMsg(self,text):
+		"""
+		It prints a message in cyan to the console with a timestamp and a prefix
+
+		Args:
+		  text: The text to be printed
+		"""
 		print(f"{Fore.CYAN}[{self.getTimeStamp()} - {self.prefix} - INFO] {text}{Style.RESET_ALL}")
 
 	def warnMsg(self,text):
+		"""
+		It prints a message in yellow with the timestamp and prefix
+
+		Args:
+		  text: The text to be printed
+		"""
 		print(f"{Fore.YELLOW}[{self.getTimeStamp()} - {self.prefix} - WARN] {text}{Style.RESET_ALL}")
 
 	def errorMsg(self,text):
+		"""
+		It prints a red error message to the console
+
+		Args:
+		  text: The text to be printed
+		"""
 		print(f"{Fore.RED}[{self.getTimeStamp()} - {self.prefix} - ERROR] {text}{Style.RESET_ALL}")
 
 	def getTimeStamp(self):
+		"""
+		It returns the current time in the format of HH:MM:SS
+
+		Returns:
+		  The current time in the format of HH:MM:SS
+		"""
 		if len(str(localtime().tm_hour)) == 1:
 			hour = f"0{localtime().tm_hour}"
 		else:
