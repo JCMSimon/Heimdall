02ce6541c3d845d6d7c4a240e6bc8e38be74dc35
Simon <simon1240.se@gmail.com>
2023-03-08 04:00:43 +0100

    Final Version

--- src/Logger.py
+++ src/Logger.py
@@ -1,69 +1,45 @@
-from colorama import Fore,Style
-from colorama import init as initColors
 from time import localtime
+
+from colorama import Fore, Style
+from colorama import init as initColors
+
 
 class Logger():
 	"""
-	Logging Utility for Plugins
+	Logging Utility for Heimdall
 	"""
-	def __init__(self,prefix,debug=False) -> None:
-		"""
-		This function initializes the class with the prefix and debug variables
-
-		Args:
-		  prefix: The prefix of the logger.
-		  debug: If set to True, it will print out the debug messages. Defaults to False
-		"""
-		self._debug = debug
-		self.prefix = prefix
+	def __init__(self,PREFIX,DEBUG=False) -> None:
+		self._DEBUG = DEBUG
+		self._PREFIX = PREFIX
+		# This method initialises colorama colors
 		initColors()
+		self.debugMsg(f"Logger instance initialised with prefix: {self._PREFIX}")
 
-	def debugMsg(self,text):
-		"""
-		It prints a message to the console if the debug flag is set to true
+	def debugMsg(self,text) -> None:
+		if self._DEBUG:
+			print(f"{Fore.BLUE}[{self.getTimeStamp()} - {self._PREFIX} - DEBUG]> {text}{Style.RESET_ALL}")
 
-		Args:
-		  text: The text to be printed
-		"""
-		if self._debug:
-			print(f"{Fore.BLUE}[{self.getTimeStamp()} - {self.prefix} - DEBUG] {text}{Style.RESET_ALL}")
+	def infoMsg(self,text) -> None:
+		print(f"{Fore.CYAN}[{self.getTimeStamp()} - {self._PREFIX} - INFO] > {text}{Style.RESET_ALL}")
 
-	def infoMsg(self,text):
-		"""
-		It prints a message in cyan to the console with a timestamp and a prefix
+	def warnMsg(self,text) -> None:
+		print(f"{Fore.YELLOW}[{self.getTimeStamp()} - {self._PREFIX} - WARN] > {text}{Style.RESET_ALL}")
 
-		Args:
-		  text: The text to be printed
-		"""
-		print(f"{Fore.CYAN}[{self.getTimeStamp()} - {self.prefix} - INFO] {text}{Style.RESET_ALL}")
+	def errorMsg(self,text) -> None:
+		print(f"{Fore.RED}[{self.getTimeStamp()} - {self._PREFIX} - ERROR]> {text}{Style.RESET_ALL}")
 
-	def warnMsg(self,text):
-		"""
-		It prints a message in yellow with the timestamp and prefix
+	def getTimeStamp(self) -> str:
+		lt = localtime()
+		# These 3 line make sure it displays 05:05:05 instead of 5:5:5 (Propper 24 Hour Format)
+		hour = str(lt.tm_hour) if len(str(lt.tm_hour)) == 2 else f"0{lt.tm_hour}"
+		minute = str(lt.tm_min) if len(str(lt.tm_min)) == 2 else f"0{lt.tm_min}"
+		second = str(lt.tm_sec) if len(str(lt.tm_sec)) == 2 else f"0{lt.tm_sec}"
+		return ":".join([hour,minute,second])
 
-		Args:
-		  text: The text to be printed
-		"""
-		print(f"{Fore.YELLOW}[{self.getTimeStamp()} - {self.prefix} - WARN] {text}{Style.RESET_ALL}")
-
-	def errorMsg(self,text):
-		"""
-		It prints a red error message to the console
-
-		Args:
-		  text: The text to be printed
-		"""
-		print(f"{Fore.RED}[{self.getTimeStamp()} - {self.prefix} - ERROR] {text}{Style.RESET_ALL}")
-
-	def getTimeStamp(self):
-		"""
-		It returns the current time in the format of HH:MM:SS
-
-		Returns:
-		  The current time in the format of HH:MM:SS
-		"""
-		if len(str(localtime().tm_hour)) == 1:
-			hour = f"0{localtime().tm_hour}"
-		else:
-			hour = localtime().tm_hour
-		return f"{hour}:{localtime().tm_min}:{localtime().tm_sec}"
+if __name__ == "__main__":
+	LoggerInstance = Logger("TEST",True)
+	LoggerInstance.debugMsg(f"{LoggerInstance},{LoggerInstance.__dict__}")
+	LoggerInstance.infoMsg("Test Info Message 123")
+	LoggerInstance.debugMsg("Test Debug Message 123")
+	LoggerInstance.warnMsg("Test Warn Message 123")
+	LoggerInstance.errorMsg("Test Error Message 123")
