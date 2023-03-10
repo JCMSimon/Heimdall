b29b98e5fe744c564b269eff5d2fee1fd3fbb0c8
JustCallMeSimon <no>
2023-03-10 16:23:20 +0100

    Core V2

--- src/Core.py
+++ src/Core.py
@@ -1,67 +1,102 @@
+import os
+import pickle
 import string
 from time import strftime
 from typing import LiteralString
-from src.Logger import Logger
+
+from tqdm import tqdm
+
 from plugins.lib.Data import datapoints as dp
 from plugins.lib.Node import Node
-import pickle
-import os
-from dearpygui.dearpygui import get_item_children,delete_item,split_frame
+from src.Logger import Logger
+
 
 class Core():
 	"""Heimdall's Core. responsible for the search process"""
 	def __init__(self,pluginRegister,DEBUG=False) -> None:
+		"""
+		This function initializes the Core class
+
+		Args:
+		  pluginRegister: This is the plugin register that the core will use to execute plugins.
+		  DEBUG: If set to True, the logger will print out debug messages. Defaults to False
+		"""
 		self._DEBUG = DEBUG
 		self.logger = Logger("Core",DEBUG=self._DEBUG)
 		self.pluginRegister = pluginRegister
 
-	def search(self,pluginName,keyword) -> None:
-		self.root = Node("ROOT", debug=self.debug).addDataField(dp._internal.is_root_node,True)
-		datapoint = self.pluginRegister.getDefaultDatapointByName(pluginName)
+	def search(self,pluginName,keyword) -> Node:
+		"""
+		> The function takes a plugin name and a keyword, and returns a list of nodes that are the result of
+		the search
 
+		Args:
+		  pluginName: The name of the plugin to run.
+		  keyword: The keyword to search for
 
+		Returns:
+		  A list of nodes.
+		"""
+		self.root = Node("ROOT", debug=self._DEBUG).addDataField(dp._internal.is_root_node,True)
+		todo = []
+		if datapoint := self.pluginRegister.getDefaultDatapointByName(pluginName) is None:
+			todo.extend(self.pluginRegister.run(pluginName,keyword))
+		else:
+			todo.extend(Node("F4K3").addDataField(datapoint,keyword))
+		return self.recursiveSearch(todo)
 
-		# self.root._children.extend(initialResults)
-		# self.todo = initialResults
-		# Recursive search
-		# while self.todo:
-		# 	for node in self.todo:
-		# 		for dataField in node.data["data"]: # this might be wrong syntax. it should loop through data fields
-		# 			for datatype,data in dataField.items():
-		# 				plugins = self.pluginRegister.getPluginNamesByType(datatype)
-		# 				results = []
-		# 				for plugin in plugins:
-		# 					try:
-		# 						results.extend(self.pluginRegister.runPlugin(plugin,data))
-		# 					except (TypeError,IndexError):
-		# 						self.logger.infoMsg(f"{plugin} returned no results")
-		# 		node._children.extend(results)
-		# 		self.todo.extend(results)
-		# 		self.todo.remove(node)
+	def recursiveSearch(self,todo,pbar) -> Node:
+		"""
+		It takes a list of nodes, and for each node, it runs all plugins that are registered for the
+		datatype of the node's data, and then adds the results of those plugins to the node's children
 
+		Args:
+		  todo: A list of nodes to process
+		  pbar: The progress bar object
+
+		Returns:
+		  A Tree of nodes
+		"""
+		pbar = getPbar(todo,"Processing Nodes","Nodes")
+		for node in todo:
+			for dataField in node.data["data"]:
+				for datatype,data in dataField.items():
+					plugins = self.pluginRegister.getPluginNamesByDatapoint(datatype)
+					results = []
+					for plugin in plugins:
+						results.extend(self.pluginRegister.runPlugin(plugin,data))
+			if node.data["title"] == "F4K3":
+				self.root._children.extend(results)
+			else:
+				node._children.extend(results)
+			pbar.update(1)
+			todo.extend(results)
+			# pbar.total += len(results)
+		pbar.close()
+		return self.root
 
 	def save(self,filename) -> None:
 		"""
-		It saves the current state of the program to a file
+		It saves the Core root to a file
 
 		Args:
 		  filename: The name of the file to save to.
 		"""
-		try:
-			filename = format_filename(filename)
-			if filename == "":
-				filename = strftime("%Y%m%d-%H%M%S")
-			path = f"./saves/{filename}.pickle"
-			self.logger.debugMsg(f"Saving to {path}")
-			split_frame()
+		filename = format_filename(filename)
+		# If the File nam would be empty, replace it with a timestamp
+		if filename == "":
+			filename = strftime("%Y%m%d-%H%M%S")
+		path = f"./saves/{filename}.pickle"
+		self.logger.debugMsg(f"Saving to {path}")
+		if self.root is None:
+			self.logger.infoMsg("No data to save")
+		else:
 			with open(path,"wb") as picklefile:
 				pickle.dump(self.root,picklefile)
-		except AttributeError:
-			self.logger.infoMsg("No data to save")
 
 	def load(self,filename) -> None:
 		"""
-		It loads a pickle file and then deletes all the nodes in the treeview.
+		It loads a save file and sets the Core root to the loaded object
 
 		Args:
 		  filename: The name of the file to load.
@@ -74,41 +109,51 @@ class Core():
 				self.root = pickle.load(picklefile)
 			except EOFError:
 				self.logger.errorMsg("Cant load empty File")
-			else:
-				for node in get_item_children(self.nodeInterFace.NE)[1]:
-					delete_item(node)
-		if self.root:
-			self.nodeInterFace.visualize(self.root)
 
 	def deleteSave(self,filename) -> bool:
 		"""
-		It deletes a file from the saves folder.
+		It deletes a save file
 
 		Args:
-		  filename: The name of the file you want to delete.
+		  filename: The name of the save file.
 
 		Returns:
-		  The return value is a boolean value.
+		  A boolean value. True if file was deleted successfuly. False if not.
 		"""
 		path = f".\saves\{filename}.pickle"
 		if os.path.exists(path):
 			os.system(f"del {path}")
 			return True
 		else:
-			self.logger.errorMsg(f"There is no Save at {path}")
+			self.logger.errorMsg(f"There is no save at {path}")
 			return False
 
 # Thanks to https://gist.github.com/seanh/93666 !
-def format_filename(s) -> LiteralString:
+def format_filename(name) -> LiteralString:
 	"""
-	It takes a string and returns a valid filename, replacing spaces with underscores
+	It takes a string and returns a string that is a valid filename
 
 	Args:
-	  s: the string to be formatted
+	  name: The string to be formatted
 
 	Returns:
-	  the filename with the spaces replaced with underscores.
+	  A string with the characters in the string s that are in the string valid_chars.
 	"""
 	valid_chars = f"-_ {string.ascii_letters}{string.digits}"
-	filename = ''.join(c for c in s if c in valid_chars)
+	filename = ''.join(c for c in name if c in valid_chars)
 	return filename.replace(' ','_')
+
+def getPbar(iterable,desc,unit,color="#7800C8") -> tqdm:
+	"""
+	`getPbar` is a function that returns a progress bar object from the `tqdm` library
+
+	Args:
+	  iterable: the iterable object you want to present
+	  desc: The description of the progress bar
+	  unit: The unit of the progress bar.
+	  color: The color of the progress bar. Defaults to #7800C8
+
+	Returns:
+	  A progress bar object.
+	"""
+	return tqdm(total=len(iterable),ascii=True,desc=desc,leave=False,unit=unit,colour=color)
